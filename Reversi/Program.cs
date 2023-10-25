64bfda8f8bceec488f0f02230f54986d68dca3cf
kovacs.gyula <kovacs.gyula@58-09>
2023-10-25 10:12:12 +0200

    Add project files.

---
+++ Reversi/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Reversi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabla ujTabla = new Tabla("allas.txt");
+            Console.WriteLine("5. feladat");
+            ujTabla.Megjelenit();
+            Console.WriteLine("6. feladat: Összegzés:");
+            Console.WriteLine($"\tKék korongok száma: {ujTabla.MezokSzama('K')}");
+            Console.WriteLine($"\tFehér korongok száma: {ujTabla.MezokSzama('F')}");
+            Console.WriteLine($"\tÜres korongok száma: {ujTabla.MezokSzama('#')}");
+            string parameterek = "F;4;1;0;1";
+            Console.WriteLine($"8. feladat: [jatekos;sor;oszlop;iranySor;iranyOszlop] = {parameterek}");
+            
+            string[] elemek = parameterek.Split(';');
+            char j = char.Parse(elemek[0]);
+            byte s = byte.Parse(elemek[1]);
+            byte o = byte.Parse(elemek[2]);
+            byte iS = byte.Parse(elemek[3]);
+            byte iO = byte.Parse(elemek[4]);
+
+            if (ujTabla.VanForditas(j, s, o, iS, iO))
+            {
+                Console.WriteLine("Van fordítás!");
+            }
+            else
+            {
+                Console.WriteLine("Nincs fordítás!");
+            }
+        }
+    }
+}
---
+++ Reversi/Tabla.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    internal class Tabla
+    {
+        char[,] t;
+
+        public Tabla(string fajlNev)
+        {
+            this.t = new char[8, 8];
+            string[] beolvasas = File.ReadAllLines(fajlNev);
+            for (int i = 0; i < beolvasas.Length; i++)
+            {
+                for (int j = 0; j < beolvasas[i].Length; j++)
+                {
+                    t[i, j] = beolvasas[i][j];
+                }
+            }
+        }
+
+        public void Megjelenit()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(t[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public byte MezokSzama(char c)
+        {
+            byte darab = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (t[i, j] == c)
+                    {
+                        darab++;
+                    }
+                }
+            }
+            return darab;
+        }
+
+        public bool VanForditas(char jatekos, byte sor, byte oszlop, byte iranySor, byte iranyOszlop)
+        {
+            int aktSor, aktOszlop;
+            char ellenfel;
+            bool nincsEllenfel;
+            aktSor = sor + iranySor;
+            aktOszlop = oszlop + iranyOszlop;
+            ellenfel = 'K';
+
+            if (jatekos == 'K')
+            {
+                ellenfel = 'F';
+            }
+            nincsEllenfel = true;
+            while (aktSor > 0 && aktSor < 8 && aktOszlop > 0 && aktOszlop < 8 && t[aktSor, aktOszlop] == ellenfel)
+            {
+                aktSor = aktSor + iranySor;
+                aktOszlop = aktOszlop + iranyOszlop;
+                nincsEllenfel = false;
+            }
+            if (nincsEllenfel || aktOszlop < 0 || aktSor > 7 || aktOszlop < 0 || aktOszlop > 7 || t[aktSor, aktOszlop] != jatekos)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsSzabalyos(char jatekos, byte sor, byte oszlop)
+        {
+            if (t[sor, oszlop] == '#')
+            {
+                bool jo = false;
+
+                return jo;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
